ffb4ab881b8816f5d7f44ec711a9a2805609eafd
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-09-01 16:07:57 +0700

    Use Package Manager to disable stock OTA
    This reverts commit ea14a88a2ac567104f267fda5a4507d0ae48040c.
    
    Using the Package Manager prevents any danling wakelock from
    killed service/receiver.
    
    Change-Id: Ie3162ca4b18a7bc9c55613af39e88ea980407e5f

--- config/common.mk
+++ config/common.mk
@@ -27,3 +27,7 @@ PRODUCT_COPY_FILES += \
     vendor/wave/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
     vendor/wave/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
 endif
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/wave/overlay/common
