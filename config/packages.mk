3b8f834c686b074669b1c7cd17f4f623a014b769
Kshitij Gupta <kshitijgm@gmail.com>
2018-05-28 21:28:00 +0000

    vendor: Add black themes (2/3)
    Change-Id: I55a47316d42df55e37b108f2bd99e82329c271d4

--- config/packages.mk
+++ config/packages.mk
@@ -25,12 +25,17 @@ PRODUCT_PACKAGES += \
     GMsgs \
     GDialer
 
-# Themes
+# Themes Dark
 PRODUCT_PACKAGES += \
     GBoardDarkTheme \
     SettingsDarkTheme \
     SystemDarkTheme \
     SystemUIDarkTheme
+
+# Themes Black
+PRODUCT_PACKAGES += \
+    SettingsBlackTheme \
+    SystemBlackTheme
 
 # Overlays
 PRODUCT_PACKAGES += \
