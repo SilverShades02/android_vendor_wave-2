a18229183393f75285a09130b083162488f8a294
LineageOS <lineageos.org>
2018-08-13 04:16:05 +0000

    add backup tool from lineage

--- config/common.mk
+++ config/common.mk
@@ -13,3 +13,17 @@ include vendor/wave/config/wave_props.mk
 
 # Packages
 include vendor/wave/config/packages.mk
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/wave/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/wave/prebuilt/common/bin/50-wave.sh:system/addon.d/50-wave.sh \
+    vendor/wave/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/wave/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/wave/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
