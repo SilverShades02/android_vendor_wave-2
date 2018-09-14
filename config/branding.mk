7da98b90c2a5bf38ada278c84f77840d42aefdf1
maxwen <max.weninger@gmail.com>
2018-09-14 17:37:15 -0500

    Add ro.wave.fingerprint property
    - Remove cache script inheritance
    
    Change-Id: If3fd8866bd88ba135f248c2a95bee31a35fb096e

--- config/branding.mk
+++ config/branding.mk
@@ -5,12 +5,16 @@ ifndef WAVE_BUILD_TYPE
     WAVE_BUILD_TYPE := UNOFFICIAL
 endif
 
+TARGET_PRODUCT_SHORT := $(subst wave_,,$(WAVE_BUILD_TYPE))
+
 # Set all versions
 DATE := $(shell date -u +%Y%m%d)
 WAVE_VERSION := $(TARGET_PRODUCT)-$(WAVE_BASE_VERSION)-$(DATE)-$(shell date -u +%H%M)-$(WAVE_BUILD_TYPE)
 TARGET_BACON_NAME := $(WAVE_VERSION)
+ROM_FINGERPRINT := WaveOS/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date +%Y%m%d)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.wave.version=$(WAVE_VERSION) \
     ro.mod.version=$(WAVE_BUILD_TYPE)-$(WAVE_BASE_VERSION)-$(DATE)
+    ro.wave.fingerprint=$(ROM_FINGERPRINT)
