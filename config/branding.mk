DirtyUnicorns/android_vendor_du

Adjust ROM fingerprint to change regularly
The purpose of the ROM fingerprint is so that we can use it to trigger a
wipe of the package cache. If the ROM fingerprint only changes once per day,
this doesn't work for us (developers) who compile sometimes up to 10-20 in
one single day. Lets add the time to the ROM fingerprint so that it triggers
the wipe on dirty flashes.

Change-Id: Id1779ec3923d9525e3f3e240a7dbc66955d90a34

config/branding.mk
@@ -22,7 +22,7 @@ endif
 DATE := $(shell date -u +%Y%m%d)
 DU_VERSION := $(TARGET_PRODUCT)-$(DU_BASE_VERSION)-$(DATE)-$(shell date -u +%H%M)-$(DU_BUILD_TYPE)
 TARGET_BACON_NAME := $(DU_VERSION)
-ROM_FINGERPRINT := DirtyUnicorns/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date +%Y%m%d)
+ROM_FINGERPRINT := DirtyUnicorns/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%H%M)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \

SilverShades02/android_vendor_wave-2

config/branding.mk
@@ -11,7 +11,7 @@ TARGET_PRODUCT_SHORT := $(subst wave_,,$(WAVE_BUILD_TYPE))
 DATE := $(shell date -u +%Y%m%d)
 WAVE_VERSION := $(TARGET_PRODUCT)-$(WAVE_BASE_VERSION)-$(DATE)-$(shell date -u +%H%M)-$(WAVE_BUILD_TYPE)
 TARGET_BACON_NAME := $(WAVE_VERSION)
-ROM_FINGERPRINT := WaveOS/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date +%Y%m%d)
+ROM_FINGERPRINT := WaveOS/$(PLATFORM_VERSION)/$(TARGET_PRODUCT_SHORT)/$(shell date -u +%H%M)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \