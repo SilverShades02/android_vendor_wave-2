DirtyUnicorns/android_vendor_du

Name our bacon package accordingly
Change-Id: Iaf96f20b5666ac49d830a2393669ac1de00777d3

config/branding.mk
@@ -19,6 +19,7 @@ endif
 # Set all versions
 DATE := $(shell date -u +%Y%m%d)
 DU_VERSION := $(TARGET_PRODUCT)-$(DU_BASE_VERSION)-$(DATE)-$(shell date -u +%H%M)-$(DU_BUILD_TYPE)
+TARGET_BACON_NAME := $(DU_VERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \

SilverShades02/android_vendor_wave-2

config/branding.mk
@@ -8,6 +8,7 @@ endif
 # Set all versions
 DATE := $(shell date -u +%Y%m%d)
 WAVE_VERSION := $(TARGET_PRODUCT)-$(WAVE_BASE_VERSION)-$(DATE)-$(shell date -u +%H%M)-$(WAVE_BUILD_TYPE)
+TARGET_BACON_NAME := $(WAVE_VERSION)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \